fa65fc633833117d4f6737a9478e4fe483cd08fc
mnotis <mnotis@princeton.edu>
2020-07-03 11:56:59 -0400

    new things

--- radial_fitting.py
+++ radial_fitting.py
@@ -178,11 +178,11 @@ def file_to_pic_curve_fit(path, file, dist_lim, f):
     
 # writes radial h5 files for all files in folder:
 def radial_h5_folder(path, n_snap):
-    dir = '/home/mnotis/FDM2020' + '/rad_files/f1.75L20T4n40r256'
-    n_snap = n_snap / 10
+    dir = '/mnt/c/Users/Noah Notis/Dropbox/USRP2020_Noah/src/FDM2020' + '/rad_files/f2L20T4n400r64'
+    n_snap = n_snap
     if not os.path.exists(dir):
         os.mkdir(dir)
-    for i in range(0, int(n_snap) + 1):
+    for i in range(400, int(n_snap)+1):
         result = file_to_rad_avg_den(path + '/snap' + str(i).zfill(4) + '.h5')
         print(i)
         dist = np.sort(result[0])
@@ -198,7 +198,7 @@ def radial_h5_folder(path, n_snap):
         hf.create_dataset('densities', data=dens)
         hf.create_dataset('rc', data=rc)
         hf.close()
-path = '/tigress/mnotis/f1.75L20T4n40r256'
+path = '/mnt/c/Users/Noah Notis/Dropbox/USRP2020_Noah/src/output/f2L20T4n400r64'
 #file_to_pic_curve_fit(path, 'snap0400.h5', 0.65, 'Inf')       
 radial_h5_folder(path, 400)
 print('Task completed successfully!')
